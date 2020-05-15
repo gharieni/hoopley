4e39b12ba3e54d1649833672e01e73828fa08019
gharieni <gmelek@students.42.fr>
2020-05-15 03:00:37 +0200

    update messanger config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+require('dotenv').config({ path: 'variables.env' });
+
 const express = require('express');
-    const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-    const app = express();
+const app = express();
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.listen(5000, () => console.log('Express server is listening on port 5000'));
-    const verifyWebhook = require('./verify-webhook');
+app.listen(5000, () => console.log('Express server is listening on port 5000'));
+const verifyWebhook = require('./verify-webhook');
 
-    app.get('/', verifyWebhook);
+app.get('/', verifyWebhook);
+const messageWebhook = require('./message-webhook');
+
+app.post('/', messageWebhook);
