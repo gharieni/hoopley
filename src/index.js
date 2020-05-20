41d0817f6ee7350f23f5461d5b3d7f95019616a4
gharieni <gmelek@students.42.fr>
2020-05-20 23:50:58 +0200

    updte server in index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(5000, () => console.log('Express server is listening on port 5000'));
-const verifyWebhook = require('./verify-webhook');
+//set server port and log message
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
 
-app.get('/', verifyWebhook);
+
+
+
+//app.listen(5000, () => console.log('Express server is listening on port 5000'));
+
+//app.get('/', verifyWebhook);
 const messageWebhook = require('./message-webhook');
 
 app.post('/', messageWebhook);
