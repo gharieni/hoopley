c262b82d9ab49e06c5320b001b89d7a8cdfdf08d
gharieni <gmelek@students.42.fr>
2020-05-21 18:51:21 +0200

    update get methode inside index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,25 @@ var server = app.listen(process.env.PORT || 5000, function () {
 //app.listen(5000, () => console.log('Express server is listening on port 5000'));
 
 //app.get('/', verifyWebhook);
-const messageWebhook = require('./message-webhook');
+//const messageWebhook = require('./message-webhook');
 
-app.post('/', messageWebhook);
+//app.post('/', messageWebhook);
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+
+  //checks this is an event from the page subscreption
+  if (body.object === 'page'{
+
+  //Iterates over each netry - there may be multiple if batched
+  body.entry.forEach(entry) {
+    //get the message enry messaging is an array , but 
+    //will only ever contain one message , so we get index 0
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
+});
+  res.status(200).send('EVENT_RECEIVED');
+} else {
+  //return a 404 not found if event is not from a page subscruption 
+  res.sendStatus(404);
+}
+});
