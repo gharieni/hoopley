e0a4b56da7cff1ef65ca91e0a38416f4b7035e71
gharieni <gmelek@students.42.fr>
2020-05-23 02:21:15 +0200

    correct index

--- src/index.js
+++ src/index.js
@@ -27,16 +27,16 @@ app.post('/webhook', (req, res) => {
   let body = req.body;
 
   //checks this is an event from the page subscreption
-  if (body.object === 'page'{
+  if (body.object === 'page'){
 
-  //Iterates over each netry - there may be multiple if batched
-  body.entry.forEach(entry) {
-    //get the message enry messaging is an array , but 
-    //will only ever contain one message , so we get index 0
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
-});
-  res.status(200).send('EVENT_RECEIVED');
+    //Iterates over each netry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      //get the message enry messaging is an array , but
+      //will only ever contain one message , so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+res.status(200).send('EVENT_RECEIVED');
 } else {
   //return a 404 not found if event is not from a page subscruption 
   res.sendStatus(404);
