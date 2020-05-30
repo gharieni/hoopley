ce35ef45125152aaae06d2fb6f4c540463e951e5
gharieni <gmelek@students.42.fr>
2020-05-31 00:11:09 +0200

    message

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.post('/webhook', (req, res) => {
   let body = req.body;
 
   //checks this is an event from the page subscreption
-  console.log('post 1')
+  console.log(res)
   if (body.object === 'page'){
   console.log('post 2')
 
