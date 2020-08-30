770bc5f8230be010977f601cdd953796923628ff
gharieni <gmelek@students.42.fr>
2020-08-30 14:33:29 +0200

    check for 4Â04 error 2

--- src/index.js
+++ src/index.js
@@ -23,6 +23,14 @@ app.post('/webhook', (req, res) => {
 
   console.log("------------------------------")
   //console.log("body")
+
+  body.entry.forEach(function(entry) {
+
+    // Gets the message. entry.messaging is an array, but
+    // will only ever contain one message, so we get index 0
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
+  });
   console.log("------------------------------")
   if (body.object === 'page'){
     console.log('post 2')
