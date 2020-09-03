6603d8646ad98690dcfe97837b7c0d194d327e7d
gharieni <gmelek@students.42.fr>
2020-09-03 17:25:56 +0200

    check for 4Â04 error 2

--- src/index.js
+++ src/index.js
@@ -15,30 +15,6 @@ var server = app.listen(process.env.PORT || 5000, function () {
   console.log("Express is working on port " + port);
 });
 
-// adds support for Get request to the webhook
-app.get('/webhook', (req, res) => {
-  //parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challange = req.query['hub.challange'];
-
-  console.log('hello 2')
-  //check if a token and mode is in the query string of the request
-  if (mode && token){
-    // checks the mode and tokn sent is correct
-    console.log('hello 3')
-    if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN)
-     //  response with the challange token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-      console.log('end if')
-    } else {
-      // Respond with 403 Forbidden if verify tokens do not match
-      res.sendStatus(403);
-      console.log('verify token do not match')
-    }
-});
-
 app.post('/webhook', (req, res) => {
   let body = req.body;
 
@@ -64,3 +40,28 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(404);
   }
 });
+
+
+// adds support for Get request to the webhook
+app.get('/webhook', (req, res) => {
+  //parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challange = req.query['hub.challange'];
+
+  console.log('hello 2')
+  //check if a token and mode is in the query string of the request
+  if (mode && token){
+    // checks the mode and tokn sent is correct
+    console.log('hello 3')
+    if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN)
+        //  response with the challange token from the request
+      console.log('WEBHOOK_VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    // Respond with 403 Forbidden if verify tokens do not match
+    res.sendStatus(403);
+    console.log('verify token do not match')
+  }
+});
+
