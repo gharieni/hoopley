6a3849bc0a152d81d0eea6765bd195a13bbd2dec
gharieni <gmelek@students.42.fr>
2020-12-23 13:18:13 +0100

    seesionPath fix

--- src/index.js
+++ src/index.js
@@ -138,7 +138,7 @@ function sendToApiAi(sender, text) {
   //
   //
   //
-  let apiaiRequest = sessionPath.textRequest(text, {
+  let apiaiRequest = apiAiService.textRequest(text, {
     sessionId: sessionIds.get(sender)
   });
 
