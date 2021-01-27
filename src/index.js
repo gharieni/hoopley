8aa1bc7eb59431e74e75fe66b6bb30e31d14b802
gharieni <gmelek@students.42.fr>
2021-01-27 09:35:22 +0100

    config callback and add file process-message and message-webhook v2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const dialogflow = require('@google-cloud/dialogflow');
-const uuid = require('uuid');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -77,73 +76,8 @@ app.post('/webhook', (req, res) => {
 */
 
 
-/* *****************************************************
- * setup dialogflow integration 
- *****************************************************  */
-
-const projectId = 'care-me-almvrf';
-const sessionId = uuid.v4();
-const languageCode = 'en-US';
-
-const config = {
-  credentials: {
-    private_key: process.env.DIALOGFLOW_PRIVATE_KEY,
-    client_email: process.env.DIALOGFLOW_CLIENT_EMAIL
-  }
-};
-
-
-const sessionClient = new dialogflow.SessionsClient(config);
-const sessionPath = sessionClient.projectAgentSessionPath(
-  projectId,
-  sessionId
-);
-const sessionIds = new Map();
-const sendTextMessage = (userId, text) => {
-  return fetch(
-  `https://graph.facebook.com/v3.0/me/messages?access_token=` + process.env.Page_Access_Token,
-    {
-      header: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        messaging_type: 'RESPONSE',
-        recipient: {
-          id: userId,
-        },
-        message: {
-          text,
-        },
-        }),
-    }
-  );
-}
-
-module.exports = (event) => {
-  const userId = event.sender.id;
-  const message = event.message.text;
-
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: message,
-        languageCode: languageCode,
-      },
-    },
-  };
-  sessionClient.detectIntent(request).then(response => {
-    const result = response[0].queryResult;
-    return sendTextMessage(userId, result.fulfillmentText);
-  })
-    .catch(err => {
-      console.error('ERROR', err);
-    });
-}
-
 const messageWebhook = require('./message-webhook');
-app.post('/', messageWebhook);
+app.post('/webhook', messageWebhook);
 /*
 function receivedMessage(event) {
   var senderID = event.sender.id;
