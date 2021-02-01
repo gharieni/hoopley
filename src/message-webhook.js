b5b2017488dae7254e613c749c8dc63655cb97b8
gharieni <gmelek@students.42.fr>
2021-02-01 10:11:52 +0100

    log response

--- src/message-webhook.js
+++ src/message-webhook.js
@@ -77,7 +77,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log('message sent!' + response)
     } else {
       console.error("Unable to send message:" + err);
     }
