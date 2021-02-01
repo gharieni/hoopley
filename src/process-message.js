aa169d45132b73514ab8d0dbc1e84d993527b7e9
gharieni <gmelek@students.42.fr>
2021-02-01 11:17:42 +0100

    check connection dialogflow messanger

--- src/process-message.js
+++ src/process-message.js
@@ -29,7 +29,10 @@ const { FACEBOOK_ACCESS_TOKEN } = process.env;
 const sendTextMessage = (userId, text) => {
   console.log("-----sendTextMessage")
   console.log(text)
-  return fetch(
+
+  callSendAPI(userId, text);        
+ // return 
+  /*fetch(
   `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`,
     {
       header: {
@@ -48,6 +51,33 @@ const sendTextMessage = (userId, text) => {
     }
   )
   .then(console.log('************** end fetch ****************'));
+  */
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  console.log(request_body.message);
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.Page_Access_Token },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+
 }
 
 module.exports = (event) => {
