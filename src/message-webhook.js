1cadaeb181873ff03c985858199bf59650ba6a3e
gharieni <gmelek@students.42.fr>
2021-02-01 09:29:10 +0100

    debug message webhook

--- src/message-webhook.js
+++ src/message-webhook.js
@@ -1,6 +1,70 @@
 const processMessage = require('./process-message');
 
+
 module.exports = (req, res) => {
+  if (req.body.object === 'page') {
+    req.body.entry.forEach(entry => {
+      //******************
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+
+
+      //**********************
+    });
+
+    res.status(200).send("event receive");
+  }
+  else{
+    console.log("404 error")
+    res.sendStatus(404);
+  }
+};
+
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*module.exports = (req, res) => {
   if (req.body.object === 'page') {
     req.body.entry.forEach(entry => {
       entry.messaging.forEach(event => {
@@ -18,3 +82,6 @@ module.exports = (req, res) => {
     res.sendStatus(404);
   }
 };
+*/
+
+
