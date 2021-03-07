56ae38a5cb1e0f80659cf95b3e0410499862a7b7
gharieni <gmelek@student.42.fr>
2021-03-07 17:43:32 +0100

    update process message 2

--- src/process-message.js
+++ src/process-message.js
@@ -97,9 +97,12 @@ module.exports = (event) => {
       },
     },
   };
+
   sessionClient.detectIntent(request).then(response => {
     const result = response[0].queryResult;
+    console.log("----------------------------");
     console.log(result);
+    console.log("----------------------------");
     return sendTextMessage(userId, result.fulfillmentText);
   })
     .catch(err => {
