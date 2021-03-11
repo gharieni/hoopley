f4c62867d4870beeb832546edfc1216e3361d7cf
gharieni <gmelek@student.42.fr>
2021-03-11 18:35:37 +0100

    update  mysql

--- src/mysql.js
+++ src/mysql.js
@@ -2,17 +2,6 @@ var mysql = require('mysql');
 
 
 
-function connectToDatabase(){
-  const connection = mysql.createConnection({
-    host : 'care-me-db.cmrrij8g9xe7.eu-west-3.rds.amazonaws.com',
-    user : 'admin',
-    password : '75lbt0u&',
-  });
-  return new Promise((resolve,reject) => {
-    connection.connect();
-    resolve(connection);
-  });
-}
 
 
 
@@ -20,7 +9,8 @@ function connectToDatabase(){
 
 
 
-  /*
+
+/*
 var sql = 'CREATE TABLE data (`age` INT , `sexe` VARCHAR(10) NOT NULL , `height` INT, `weight` INT, `contact` VARCHAR(100), `relation` VARCHAR(100), `pcr` VARCHAR(10),`pathologie` VARCHAR(100))';
 connection.query(sql,  function (err, res) {
   if(err) throw err;
@@ -28,20 +18,20 @@ connection.query(sql,  function (err, res) {
 });
 
 
- */
+*/
 function queryDatabase(author){
   connection.query('INSERT INTO data SET ?', author, function (err, res) {
     if(err) throw err;
     console.log('Last insert ID:', res.insertId);
   });
-console.log('_________________________________________________');
+  console.log('_________________________________________________');
   connection.query("SELECT * FROM todos", function (err,rows) {
     if(err) throw err;
     console.log('Data received from Db:');
     console.log(rows);
   });
- 
-connection.end();
+
+  connection.end();
 }
 
 
@@ -49,6 +39,20 @@ var author = {age: '', sexe: '', height: '', weight: '', contact: '', relation: 
 
 var pushToMysql = (userId, intent, text) => {
 
+
+
+
+  function connectToDatabase(){
+    const connection = mysql.createConnection({
+      host : 'care-me-db.cmrrij8g9xe7.eu-west-3.rds.amazonaws.com',
+      user : 'admin',
+      password : '75lbt0u&',
+    });
+    return new Promise((resolve,reject) => {
+      connection.connect();
+      resolve(connection);
+    });
+  }
   console.log('function push mysql');
   console.log(intent.displayName);
 
