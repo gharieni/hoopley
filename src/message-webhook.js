aa316d889aa496f65a92fa2dda0a35601c4aee44
gharieni <gmelek@students.42.fr>
2021-03-31 15:15:26 +0200

    check 404 error

--- src/message-webhook.js
+++ src/message-webhook.js
@@ -13,7 +13,10 @@ module.exports = (req, res) => {
     res.status(200).send("event receive");
   }
   else{
-    console.dir(req.body.entry);
-    res.sendStatus(404);
+    console.dir(res);
+  //  if (result.intent) {
+   //   pushToMysql(userId, result.intent, result.queryText);
+   // }
+    res.sendStatus(201);
   }
 }
